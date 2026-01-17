3b88784c0d96b398d327a3cf8941f2acc50b10f2
Dawid P. <pater.dawid92@gmail.com>
2026-01-17 23:56:41 +0100

    LiveQuery example.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,20 @@
+import { ingredientCategories } from "@/db/schema";
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import { useSQLiteContext } from "expo-sqlite";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db);
+  useDrizzleStudio(db);
+
+  const update = useLiveQuery(
+    drizzleDb.select().from(ingredientCategories)
+  );
+
+  console.log(update);
+
   return (
     <View
       style={{
